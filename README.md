3becbe93794bb8df2ad4ce28087458ec6a0aa022
DerekGn <dg89327@hotmail.com>
2018-11-14 16:23:45 +0000

    Initial migrated version

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # NativeUsbLib
+
+This is a port of the excellent .net Usb Library from the sourceforge project [
+UsbViewer in C#](https://sourceforge.net/projects/usbviewerincsha/) by [nickvetter](https://sourceforge.net/u/nickvetter/profile/). The library has not been updated for quite some time and is targeting .net framework 2.0. There are also issues running on x64 builds of Windows. This repo updates the runtimes and resolves the x64 runtime issues. The library is also available on [nuget.org](https://www.nuget.org/packages/NativeUsbLib).
+
+## Installing NativeUsbLib
+
+Install the NativeUsbLib package via nuget package manager console:
+
+```powershell
+Install-Package NativeUsbLib
+```
+
+## Supported .Net Runtimes
+
+* .net 2.0
+* .net 4.5
+* .net 4.6
+
+## Supported Operating Systems
+
+* Windows 7 x86/x64
+* Windows 8 x86/x64
+* Windows 10 x86/x64
---
+++ src/EnumerateUsb/Program.cs
@@ -0,0 +1,95 @@
+﻿using NativeUsbLib;
+using System;
+
+namespace EnumerateUsb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UsbBus usbBus = new UsbBus();
+
+                foreach (UsbController controller in usbBus.Controller)
+                    ShowController(controller);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void ShowController(UsbController controller)
+        {
+            if (controller != null)
+            {
+                Console.WriteLine($"Controller: [{controller.DeviceDescription}]");
+
+                foreach (UsbHub hub in controller.Hubs)
+                {
+                    ShowHub(hub);
+                }
+
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Controller  = null");
+            }
+        }
+
+        private static void ShowHub(UsbHub hub)
+        {
+            if (hub != null)
+            {
+                if (hub.IsRootHub)
+                {
+                    Console.WriteLine($"[{hub.DeviceDescription}]");
+                }
+                else
+                {
+                    Console.WriteLine($"Port[{hub.AdapterNumber}] DeviceConnected: {hub.DeviceDescription}");
+                }
+
+                foreach (Device device in hub.Devices)
+                {
+                    ShowDevice(device);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hub = null");
+            }
+        }
+
+        private static void ShowDevice(Device device)
+        {
+            if (device != null)
+            {
+                if (device is UsbHub)
+                {
+                    ShowHub((UsbHub)device);
+                }
+                else
+                {
+                    string s = "Port[" + device.AdapterNumber + "]";
+                    if (device.IsConnected)
+                    {
+                        s += " DeviceConnected: " + device.DeviceDescription;
+                    }
+                    else
+                    {
+                        s += " NoDeviceConnected";
+                    }
+
+                    Console.WriteLine(s);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Device = null");
+            }
+        }
+    }
+}
